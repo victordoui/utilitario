40213d4749e49869f88fb42ae7a1ed547f1ddd9e
Victor Souza da Silva <victordoui@gmail.com>
2021-11-04 15:39:28 -0300

    Atualizações 04-11-2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'utilitario';
+
+  textoOriginal = '';
+  textoTratado = '';
+
+  tratamentoDoProcesso(textoOriginal: string) {
+
+    const quantidadeCaracteres = 2;
+    const pedacos = this.quebrarPedacos(textoOriginal, quantidadeCaracteres);
+    this.textoTratado = this.tratarPedacos(pedacos);
+  }
+
+  private quebrarPedacos(textoOriginal: string, quantidadeCaracteres: number): string[] {
+    let pedacos = [];
+    let ind = 0;
+    for (let i = 0; i < textoOriginal.length / quantidadeCaracteres; i++) {
+      let pedacoTexto = textoOriginal.substring(ind, ind + quantidadeCaracteres);
+      pedacos.push(pedacoTexto);
+      ind += quantidadeCaracteres;
+    }
+    return pedacos;
+  }
+
+  private tratarPedacos(pedacos: any[]) {
+
+    let montandoTexto = `
+DECLARE string_result CLOB;
+
+BEGIN
+`;
+
+    for (const umDosPedacos of pedacos) {
+      montandoTexto += `
+DBMS_LOB.WRITEAPPEND(
+
+
+  string_result,
+  length('${umDosPedacos}'),
+  '${umDosPedacos}'
+);\n`
+    }
+
+    montandoTexto += `
+update ur_hbl.CD_BLCO_INFO_CNTEUD_HBL
+set TE_BLCO_INFO_CNTEUD = string_result
+where ID_BLCO_INFO_CNTEUD = xxx;
+
+commit;`;
+
+    return montandoTexto;
+  }
 }
